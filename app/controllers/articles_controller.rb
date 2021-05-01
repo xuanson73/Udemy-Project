7a105363707a2b278d86d4a5706c2505816753d5
xuanson73 <xuanson070399@gmail.com>
2021-05-02 00:29:55 +0700

    1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,50 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
     
     def show
-        @article = Article.find_by(params[:id])
     end
 
     def new
+        @article= Article.new
+    end
+
+    def edit
     end
 
     def create
-        @article = Article.new(article_params)
         if @article.save
+            flash[:notice]= "article was successfully created"
             redirect_to @article
         else 
-            render :new
+            render 'new'
         end
     end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice]= "article was successfully updated"
+         redirect_to @article
+        else
+            render 'edit'  
+        end
+            
+    end
+
+    def destroy
+        @article.destroy
+        flash[:notice]= "article was successfully destroyed"
+        redirect_to articles_path
+    end
+
     private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
 
     def article_params
         params.require(:article).permit(:title, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => "pages#about"
-  resources :articles, only: [:show,:index,:new,:create]
-
+  resources :articles
 end
